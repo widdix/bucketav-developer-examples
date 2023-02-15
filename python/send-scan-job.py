3937b8fa648d26ff990b7adbedd601048c7814af
Michael Wittig <michael@widdix.de>
2023-02-15 10:46:45 +0100

    fix

--- python/send-scan-job.py
+++ python/send-scan-job.py
@@ -16,6 +16,14 @@ def scan(queue_url, bucket, key):
         })
     )
 
+
+# To get the Scan Queue URL:
+# 1. Visit https://console.aws.amazon.com/cloudformation/
+# 2. Ensure that you are in the correct region.
+# 3. Navigate to Stacks.
+# 4. Click on the bucketAV stack (if you followed our docs, the name is bucketav).
+# 5. Click on the Outputs tab.
+# 6. Use the value next to the output key ScanQueueUrl.
 scan('your-scan-queue-url', 'your-bucket', 'path/to/file.pdf')
 
 print('Done')
